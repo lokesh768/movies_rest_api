5cebb6371e677ca476ec7d09e7b9c503a73db7de
lokesh768 <kadavakallulokesh@gmail.com>
2024-10-18 22:04:17 +0530

    movies,action,thriller endpoints created

---
+++ movies/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieSerializer
+from .models import Moviedata
+
+# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.all()
+    serializer_class = MovieSerializer
+
+class ActionViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.filter(genre='action')
+    serializer_class = MovieSerializer
+
+class ThrillerViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.filter(genre='thriller')
+    serializer_class = MovieSerializer
