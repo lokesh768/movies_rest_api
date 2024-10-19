577dea448aa332dfc86b2b7eb6ea24cb99a83568
lokesh768 <kadavakallulokesh@gmail.com>
2024-10-19 16:25:11 +0530

    image field added to api

--- drf_project/urls.py
+++ drf_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from movies.views import MovieViewSet, ActionViewSet, ThrillerViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 # setting up routes
 router = routers.SimpleRouter()
@@ -28,4 +30,4 @@ router.register('thriller', ThrillerViewSet, basename='thriller')
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- movies/models.py
+++ movies/models.py
@@ -14,3 +14,7 @@ class Moviedata(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     genre = models.CharField(max_length=100, default='action')
+    image = models.ImageField(
+        upload_to='Images/',
+        default="Images/None/noimg.jpg"
+    )
